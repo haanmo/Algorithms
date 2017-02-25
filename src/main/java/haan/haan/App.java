04a94ea1876a510080f2fad419b83e023169c980
Gon soo Moon <gonsoomoon@icloud.com>
2017-02-25 10:33:59 -0600

    Gonsoo’s commit

--- src/main/java/haan/haan/App.java
+++ src/main/java/haan/haan/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("생일 축하해");
         System.out.println("What is good for today's lunch?");
         System.out.println("It is very tricky");
+        System.out.println("Today is wonderful day for us");        
     }
 }
