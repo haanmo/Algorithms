09f078b865fd2ef0013726c3c11fb6ffe7108e96
KWANG HOON AN <kwang3618@gmail.com>
2017-02-25 10:36:01 -0600

    a

--- src/main/java/haan/haan/App.java
+++ src/main/java/haan/haan/App.java
@@ -14,6 +14,8 @@ public class App
         System.out.println("생일 축하해");
         System.out.println("What is good for today's lunch?");
         System.out.println("It is very tricky");
-        System.out.println("Today is wonderful day for us");        
+        System.out.println("Today is wonderful day for us");
+        
+        System.out.println("HaHa");
     }
 }
